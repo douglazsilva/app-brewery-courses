de8aadc0b43e8639be7ddf0e0e462b0007a143f4
douglazsilva <fdcsone@hotmail.com>
2020-04-14 12:35:07 -0300

    tapping into widget properties

---
+++ introducing-to-flutter-development-bootcamp-using-dart/apps/mi_card_flutter/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/douglaz.jpg'),
+              ),
+              Text(
+                'Douglaz Silva',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
